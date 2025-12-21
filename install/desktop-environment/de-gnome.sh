e2b7ddcb21ba0a1b3fbbe8c65646dbb64ea55d66
Matthew Labrecque <matthew.labrecque@proton.me>
2025-12-21 17:08:32 -0500

    Added calculator

--- install/desktop-environment/de-gnome.sh
+++ install/desktop-environment/de-gnome.sh
@@ -2,3 +2,6 @@
 
 # Loupe is the default image viewer for GNOME
 sudo pacman -S --noconfirm loupe
+
+# GNOME Calculator
+sudo pacman -S --noconfirm gnome-calculator
--- install/desktop-environment/de-kde.sh
+++ install/desktop-environment/de-kde.sh
@@ -2,3 +2,6 @@
 
 # Gwenview is an image viewing application for KDE
 sudo pacman -S --noconfirm gwenview
+
+# Kcalc calculator tool
+sudo pacman -S --noconfirm kcalc
