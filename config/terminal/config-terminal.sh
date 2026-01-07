4d9e95d7ff2470564b29614cfb95ec96a2c347a9
Matthew Labrecque <matthew.labrecque@proton.me>
2026-01-06 23:40:40 -0500

    Added FiraCode nerd font to install via pacman

--- config/terminal/config-terminal.sh
+++ config/terminal/config-terminal.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
 # Switch the default terminal to zsh
-chsh -s $(which zsh)
+chsh -s "$(which zsh)"
 
 # Add oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Add starship to terminal
 sudo pacman -S --noconfirm starship
-
-# Append Ghostty configuration with additional parameters
-mkdir -p ~/.config/ghostty
-cat <<EOF >>~/.config/ghostty/config
-#Shell configuration
-command = $(which zsh)
-
-#Font configuration
-font-family = "FiraCode Nerd Font Mono"
-font-size = 12
-maximize = true
-theme = Atom One Dark
-EOF
 
 # Append .zshrc file with additional parameters
 cat <<EOF >>~/.zshrc
---
+++ install/terminal/terminal-font.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -S --noconfirm ttf-firacode-nerd
