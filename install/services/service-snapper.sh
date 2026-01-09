51242a1b1ca65c233661a9d5aaf3e02d160deb86
Matthew Labrecque <matthew.labrecque@proton.me>
2026-01-08 22:49:55 -0500

    Added applications

--- install/services/service-snapper.sh
+++ install/services/service-snapper.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Snapper is a utility for creating system snapshots for easy rollback of updates if something breaks
-# I also install the btrfs-assistant to enable a GUI front end for snapshots
-sudo pacman -S --noconfirm snapper btrfs-assistant
+sudo pacman -S --noconfirm snapper
